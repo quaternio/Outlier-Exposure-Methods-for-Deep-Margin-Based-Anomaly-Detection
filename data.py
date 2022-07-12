2436b6fd2f05221fd956075067f612d04c211e0a
tnoel20 <thomas.c.noel@gmail.com>
2022-07-12 08:57:09 -0700

    Added dataloaders to model pipeline in experiment.py

--- data.py
+++ data.py
@@ -30,90 +30,90 @@ To brainstorm a bit...
 '''
 
 import torch
+import random
 import pickle as pkl
+import numpy as np
+import copy
 
+import torchutil.data
 import torchutil
+from adtools.data.split import ClassSplit
+from random_split_generator import FourWayClassSplit
+from torch.utils.data import ConcatDataset
+from torch.utils.data import random_split
 
-with open('id-ood-splits/split1.pkl', 'rb') as f:
-    id_ood_split = pkl.load(f) # Type is adtools.data.ClassSplit
+def build_split_datasets(split):
+    assert(split < 5 and split >= 0)
+    with open('id-ood-splits/split{}.pkl'.format(split), 'rb') as f:
+        id_ood_split = pkl.load(f) # Type is adtools.data.FourWayClassSplit
 
-print(f'ID classes: {id_ood_split.id_labels()}')
-print(f'OOD classes: {id_ood_split.ood_labels()}')
+    # Wrapper around torchvision.datasets.CIFAR100 which provides an interface for
+    # getting image labels without loading images from disk (significantly speeds
+    # up ID/OOD splitting process)
+    train_dataset = torchutil.data.CIFAR100(root = 'data', train = True, download = True)
+    test_dataset = torchutil.data.CIFAR100(root = 'data', train = False, download = True)
 
-# Wrapper around torchvision.datasets.CIFAR100 which provides an interface for
-# getting image labels without loading images from disk (significantly speeds
-# up ID/OOD splitting process)
-train_dataset = torchutil.data.CIFAR100(root = '/scratch/guyera/anomaly_detection/datasets', train = True, download = True)
-test_dataset = torchutil.data.CIFAR100(root = '/scratch/guyera/anomaly_detection/datasets', train = False, download = True)
+    id_train_dataset, ood_train_dataset, ood_val1_dataset, ood_test1_dataset = id_ood_split.split_dataset(train_dataset)
+    id_test_dataset, _, ood_val2_dataset, ood_test2_dataset = id_ood_split.split_dataset(test_dataset)
 
-id_train_dataset, ood_train_dataset = id_ood_split.split_dataset(train_dataset)
-id_test_dataset, ood_test_dataset = id_ood_split.split_dataset(test_dataset)
-
-# Map ID labels and OOD labels to [0, 1, 2, ...]
-id_train_dataset = torchutil.data.TransformingDataset(
-    id_train_dataset,
-    target_transform = torchutil.data.LabelMappingTransform(
-        label_list = id_ood_split.id_labels()
+    # Map ID labels and OOD labels to [0, 1, 2, ...]
+    id_train_dataset = torchutil.data.TransformingDataset(
+        id_train_dataset,
+        target_transform = torchutil.data.LabelMappingTransform(
+            label_list = id_ood_split.id_labels()
+        )
     )
-)
-ood_train_dataset = torchutil.data.TransformingDataset(
-    ood_train_dataset,
-    target_transform = torchutil.data.LabelMappingTransform(
-        label_list = id_ood_split.ood_labels()
+
+    id_train_len = round(0.8 * len(id_train_dataset))
+    id_val_len   = len(id_train_dataset) - id_train_len
+
+    id_train_dataset, id_val_dataset = random_split(id_train_dataset, 
+                                            [id_train_len, id_val_len], 
+                                            generator=torch.Generator().manual_seed(42))
+
+    id_test_dataset = torchutil.data.TransformingDataset(
+        id_test_dataset,
+        target_transform = torchutil.data.LabelMappingTransform(
+            label_list = id_ood_split.id_labels()
+        )
     )
-)
-id_test_dataset = torchutil.data.TransformingDataset(
-    id_test_dataset,
-    target_transform = torchutil.data.LabelMappingTransform(
-        label_list = id_ood_split.id_labels()
+
+    ood_train_dataset = torchutil.data.TransformingDataset(
+        ood_train_dataset,
+        target_transform = torchutil.data.LabelMappingTransform(
+            label_list = id_ood_split.ood_train_labels()
+        )
     )
-)
-ood_test_dataset = torchutil.data.TransformingDataset(
-    ood_test_dataset,
-    target_transform = torchutil.data.LabelMappingTransform(
-        label_list = id_ood_split.ood_labels()
+
+    ood_val1_dataset = torchutil.data.TransformingDataset(
+        ood_val1_dataset,
+        target_transform = torchutil.data.LabelMappingTransform(
+            label_list = id_ood_split.ood_val_labels()
+        )
     )
-)
 
-# Get indices of "training classes", which is a subset of the 90 ID classes,
-# and a superset of the "evaluation classes"
-num_training_classes = 90 # In this example, we use all 90 ID classes for training
-training_class_indices = list(range(num_training_classes))
+    ood_test1_dataset = torchutil.data.TransformingDataset(
+        ood_test1_dataset,
+        target_transform = torchutil.data.LabelMappingTransform(
+            label_list = id_ood_split.ood_test_labels()
+        )
+    )
 
-# Get indices of "evaluation classes", which is a subset of the training
-# classes. Basically, the evaluation classes are our true ID classes, and the
-# training classes are a mix of ID classes and other "auxiliary" classes.
-num_evaluation_classes = 10 # These are your ID classes
-evaluation_class_indices = list(range(num_evaluation_classes))
+    ood_val2_dataset = torchutil.data.TransformingDataset(
+        ood_val2_dataset,
+        target_transform = torchutil.data.LabelMappingTransform(
+            label_list = id_ood_split.ood_val_labels()
+        )
+    )
 
-# Filter the ID data to only include the training classes. They're already
-# in the range [0, 1, ...], so no label remapping is necessary.
-id_train_dataset = torchutil.data.Subset(id_train_dataset, torchutil.data.get_indices_of_labels(id_train_dataset, training_class_indices))
+    ood_test2_dataset = torchutil.data.TransformingDataset(
+        ood_test2_dataset,
+        target_transform = torchutil.data.LabelMappingTransform(
+            label_list = id_ood_split.ood_test_labels()
+        )
+    )
 
-# Evaluate only on the evaluation classes.
-id_test_dataset = torchutil.data.Subset(id_test_dataset, torchutil.data.get_indices_of_labels(id_test_dataset, evaluation_class_indices))
+    ood_val_dataset  = ConcatDataset([ood_val1_dataset, ood_val2_dataset])
+    ood_test_dataset = ConcatDataset([ood_test1_dataset, ood_test2_dataset])
 
-'''
-You may want to split the train dataset into train / val before filtering it
-to the training classes. Then you can separately filter it to the training
-classes, and filter the val set to the evaluation classes (unless you want to
-perform model selection based on the classification performance on all of
-the training classes; that's an important design decision)
-
-Of course, you'll likely want to modify your anomaly detector to understand
-the difference between training classes and evaluation classes. Training classes
-are a union of evaluation classes and auxiliary classes. When you're doing
-anomaly detection, you usually only want to look at the evaluation classes.
-In the case of max logit, I usually do the following in the score() function
-of my anomaly detector class:
-
-1. Make the evaluation_class_indices an argument to the function
-2. Compute `logits = self.model(batch)`
-3. Filter the logits to only include the logits of the evaluation classes: `logits = logits[:, evaluation_class_indices]`
-4. Compute the max filtered logit: `max_logits, _ = torch.max(logits, dim = 1)`
-5. The score is the negative max logit: `score = -max_logits`
-
-The strategy to incorporate auxiliary classes will probably depend on the nature
-of the anomaly detection method, but I think in general it will look something
-like that.
-'''
+    return (id_train_dataset, id_val_dataset, id_test_dataset), (ood_train_dataset, ood_val_dataset, ood_test_dataset)
--- experiment.py
+++ experiment.py
@@ -15,6 +15,9 @@ import numpy as np
 from large_margin import LargeMarginLoss
 import time
 import argparse
+from data import build_split_datasets
+import pickle as pkl
+from random_split_generator import FourWayClassSplit
 
 def test(model, test_loader):
     model.eval()
@@ -177,16 +180,12 @@ def main():
                         help="Which type of anomaly detection to use ('KS' or 'LS')", type=str)
     parser.add_argument("-t", "--test", action="store_true", 
                         help="Indicates that we wish to test instead of validate model.")
-    parser.add_argument("-i", "--num_id_classes", type=int, default=10,
-                        help="The number of in-distribution classes to use during training")
-    parser.add_argument("-o", "--num_oe_classes", type=int, default=10,
-                        help="Number of classes used for outlier exposure during training.")
-    parser.add_argument("--num_novel_val_classes", type=int, default=10,
-                        help="Number of unseen outlier classes in validation set")
-    parser.add_argument("--num_novel_test_classes", type=int, default=70,
-                        help="Number of unseen outlier classes in test data")
-    parser.add_ar
-
+    parser.add_argument("-o", "--optimizer", type=str, default="sgd")
+    parser.add_argument("--learning_rate", type=float, default=0.001)
+    parser.add_argument("-m", "--momentum", type=float, default=0.9)
+    parser.add_argument("-b", "--batch_size", type=int, default=256)
+    parser.add_argument("-s", "--split", type=int, default=0)
+    parser.add_argument("-e", "--num_epochs", type=int, default=5)
     # TODO: Finish adding arguments; start instrumenting for weights and biases sweep
     args = parser.parse_args()
 
@@ -196,35 +195,48 @@ def main():
     # Setup Weights and Biases and specify hyperparameters
     wandb.init(project="Thomas-Masters-Project")
 
-    learning_rate = 0.001
-    epochs = 5
-    batch_size = 256
-    net_type = "pretrained_resnet18"
-    num_training_classes = 100
-
     wandb.config = {
-        "learning_rate": learning_rate,
-        "epochs": epochs,
-        "batch_size": batch_size,
-        "network": net_type
+        "learning_rate": args.learning_rate,
+        "epochs": args.epochs,
+        "batch_size": args.batch_size,
+        "network": args.architecture
     }
 
-    train_loader = data.DataLoader(
-            datasets.CIFAR100('./data', train=True, download=True,
-                        transform=transforms.Compose([
-                            transforms.ToTensor(),
-                            transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229, 0.224, 0.225])
-                        ])),
-            batch_size=batch_size, shuffle=True, drop_last=True)
+    split = args.split
+    learning_rate = args.learning_rate
+    epochs = args.num_epochs
+    batch_size = args.batch_size
+    net_type = args.architecture
 
-    test_loader = data.DataLoader(
-            datasets.CIFAR100('./data', train=False,
-                        transform=transforms.Compose([
-                            transforms.ToTensor(),
-                            transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229, 0.224, 0.225])
-                        ])),
-            batch_size=batch_size, shuffle=False, drop_last=False)
+    # Hard-coded for now. To change, re-generate splits via random_split_generator.py
+    num_training_classes = 10
 
+    # Getting split definition
+    with open('id-ood-splits/split{}.pkl'.format(split), 'rb') as f:
+        id_ood_split = pkl.load(f) # Type is adtools.data.FourWayClassSplit
+
+    # Any label not in id_labels is OOD
+    id_labels = id_ood_split.id_labels()
+
+    # Right now, hardcoded to use CIFAR-100, with hardcoded splits
+    # defined and generated in random_split_generator.py
+    id_data, ood_data = build_split_datasets(split)
+
+    id_train_data, id_val_data, id_test_data    = id_data
+    ood_train_data, ood_val_data, ood_test_data = ood_data
+
+    # Constructing Dataloaders
+
+    id_train_loader = data.DataLoader(id_train_data, batch_size=batch_size, shuffle=True, drop_last=True)
+    id_val_loader   = data.DataLoader(id_val_data, batch_size=batch_size, shuffle=True, drop_last=True)
+    id_test_loader   = data.DataLoader(id_test_data, batch_size=batch_size, shuffle=True, drop_last=True)
+    ood_train_loader   = data.DataLoader(ood_train_data, batch_size=batch_size, shuffle=True, drop_last=True)
+    ood_val_loader   = data.DataLoader(ood_val_data, batch_size=batch_size, shuffle=True, drop_last=True)
+    ood_test_loader   = data.DataLoader(id_test_data, batch_size=batch_size, shuffle=True, drop_last=True)
+
+    #################
+    # LOSS FUNCTION #
+    #################
     lm = LargeMarginLoss(
         gamma=10000,
         alpha_factor=4,
@@ -235,15 +247,19 @@ def main():
     net = FeatureExtractor(net_type, num_training_classes)
     net.to(device)
 
+    # Data and Net accounted for; Note that net will need to be modified later for 
+    # kitchen sink configuration. (The number of classes will just be 
+    # num_training_classes+1)
+
     optim = Adam(net.parameters()) #SGD(net.parameters(), lr=learning_rate, momentum=0)
     for i in range(0, epochs):
         start_time = time.time()
-        train_lm(net, train_loader, optim, i, lm)
+        train_lm(net, id_train_loader, optim, i, lm)
         end_time = time.time()
 
         print('Epoch {} took {} seconds to complete'.format(i+1, end_time-start_time))
 
-        test(net, test_loader)
+        test(net, id_test_loader)
 
 if __name__ == '__main__':
     main()
